9fdf67aa586dbaf221e39360d65a7c22e5ec59d5
Chinsyo <wangchenxiaomine@gmail.com>
2021-05-25 19:33:32 +0800

    rename folders

--- .github/scripts/readme.py
+++ .github/scripts/readme.py
@@ -4,8 +4,8 @@ from os import getcwd, listdir, remove
 from os.path import isfile, join, splitext
 
 README = "README.md"
-SRCDIR = join(getcwd(), 'src')
-IMGDIR = join(getcwd(), 'img')
+SOURCE_DIR = join(getcwd(), 'source')
+ASSETS_DIR = join(getcwd(), 'assets')
 
 def clean():
     try:
@@ -13,32 +13,32 @@ def clean():
     except:
         pass
 
-def listresources():
-    srcs = [f for f in listdir(SRCDIR) if isfile(join(SRCDIR, f))]
-    imgs = [f for f in listdir(IMGDIR) if isfile(join(IMGDIR, f))]
+def list_files():
+    srcs = [f for f in listdir(SOURCE_DIR) if isfile(join(SOURCE_DIR, f))]
+    imgs = [f for f in listdir(ASSETS_DIR) if isfile(join(ASSETS_DIR, f))]
     srcs.sort()
     imgs.sort()
     return srcs, imgs
 
-def writetoreadme(text):
+def write_to_readme(text):
     with open(README, 'a') as f:
         f.write(text)
 
-def generatetitle(srcs):
+def update_title(srcs):
     title = "# commandline"
     title += "\n"
     title += "{} useful bash scripts collection on Mac OSX.(实用的命令行脚本)".format(len(srcs))
     title += "\n"
     return title
 
-def generatecontent(src, has_img):
+def update_content(src, has_img):
     src_name, src_ext = splitext(src)
     img_name, img_ext = src_name, '.png'
     
     text = "* {}".format(src_name)
     text += "\n"
-    text += "![{}]({})\n".format(img_name, join("img", img_name + img_ext)) if has_img else ""
-    with open(join(SRCDIR, src), 'r') as f:
+    text += "![{}]({})\n".format(img_name, join(ASSETS_DIR, img_name + img_ext)) if has_img else ""
+    with open(join(SOURCE_DIR, src), 'r') as f:
         text += "```bash"
         text += "\n"
         text += f.read()
@@ -51,15 +51,15 @@ def generatecontent(src, has_img):
 
 def main():
     clean()
-    srcs, imgs = listresources()
-    title = generatetitle(srcs)
-    writetoreadme(title)
+    srcs, imgs = list_files()
+    title = update_title(srcs)
+    write_to_readme(title)
     for src in srcs:
         src_name, src_ext = splitext(src)
         img_name, img_ext = src_name, '.png'
         has_img = (img_name + img_ext) in imgs
-        content = generatecontent(src, has_img)
-        writetoreadme(content)
+        content = update_content(src, has_img)
+        write_to_readme(content)
 
 if __name__ == '__main__':
     main()
