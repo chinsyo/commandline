dc35976c4d5fdad24620c4ba181a09d90ddf9f81
Chinsyo <chinsyo@sina.cn>
2021-11-27 09:36:34 +0800

    add systab.sh

---
+++ source/systab.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+# Copy from https://github.com/hollance/neural-engine/blob/master/docs/is-model-using-ane.md
+
+image list IMAGE_NAME
+
+# This prints the path to the IMAGE_NAME framework, something like this (it will be different on your machine):
+
+image dump systab 'put-the-path-here'
